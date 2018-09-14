5a60125db782b5352ca5a985d8a199962fd84db4
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-09-13 18:40:04 -0700

    condense dcomputerNetwork.js a bit

--- Company Challenges/computerNetwork/computerNetwork.js
+++ Company Challenges/computerNetwork/computerNetwork.js
@@ -2,18 +2,17 @@ function computerNetwork(n, network) {
     let current = 1;
     let distance = 0;
     
-    network = network.sort((a, b) => a[2] - b[2]);
-    
     const trackDistance = (index, n) => {
         distance += network[index][2];
         current = network[index][n];
         return -1;
     }
+    
+    network = network.sort((a, b) => a[2] - b[2]);
 
     for (let i = 0; current < n; i++) { 
-        if (network[i][0] < current || network[i][1] < current) continue;
-        if (network[i][1] != current && network[i][0] == current) i = trackDistance(i, 1);
-        else if (network[i][0] != current && network[i][1] == current) i = trackDistance(i, 0);        
+        if (network[i][1] > current && network[i][0] == current) i = trackDistance(i, 1);
+        else if (network[i][0] > current && network[i][1] == current) i = trackDistance(i, 0);        
     }
     
     return distance;    
