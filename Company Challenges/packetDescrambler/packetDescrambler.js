d0b27bb3f011c7682519dfbb86a378cfd18f535a
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-09-14 13:02:43 -0700

    add packetDescrambler
    Passed 13/18 tests

---
+++ Company Challenges/packetDescrambler/packetDescrambler.js
@@ -0,0 +1,39 @@
+function packetDescrambler(seq, fragmentData, n) {
+    let goal, current, count;
+    let unique = [...new Set(seq)].sort((a, b) => a - b);
+    if (unique[unique.length - 1] - unique[0] + 1 != unique.length) return '';
+    
+    let message = new Array(unique.length);
+
+    let hash = {};
+    for (let i = 0; i < seq.length; i++) {
+        if (hash[seq[i]]) hash[seq[i]].push(fragmentData[i]);
+        else hash[seq[i]] = [fragmentData[i]];
+    }
+        
+    for (i in hash) {
+        goal = Math.ceil(hash[i].length / 2);        
+        current = '';
+        count = 0;
+        
+        for (let j = 0; j < hash[i].length; j++) {
+            if (j != 0) {
+                if (hash[i][j] == current) {
+                    count++;
+                    if (count == goal) break;
+                } else current = hash[i][j]; 
+            } else {
+                current = hash[i][j];
+                count++;
+            }
+        }
+        
+        if (count <= n / 2) return ''; 
+        message[i] = current;
+    }
+    
+    if (message.length == 1 && message[0] == '#') return '#';
+    if (message[0] == '#' || message[0] == '+') return '';
+    
+    return message.join('');
+}
