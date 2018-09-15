95a645603053e7e53a13c72a346c97495887391b
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-09-14 22:06:47 -0700

    17/18 tests passing

--- Company Challenges/packetDescrambler/packetDescrambler.js
+++ Company Challenges/packetDescrambler/packetDescrambler.js
@@ -1,83 +1,31 @@
 function packetDescrambler(seq, fragmentData, n) {
-    let message = [];
     let hash = {};
-    let last = seq[0];
-    let goal = Math.ceil(n / 2);        
-    
-    for (let i = 0; i < seq.length; i++) {
-        if (hash[seq[i]]) hash[seq[i]].push(fragmentData[i]);
-        else hash[seq[i]] = [fragmentData[i]];
-    }
-            
-    for (i in hash) {
-        current = '';
-        count = 0;
+    let char, most;
+    let message = "";
+    let mLength = Math.max(...seq) - Math.min(...seq) + 1;
         
-        for (let j = 0; j < hash[i].length; j++) {
-            if (j) {
-                if (hash[i][j] == current) {
-                    count++;
-                    if (count == goal) break;
-                } else current = hash[i][j]; 
-            } else {
-                current = hash[i][j];
-                count++;
+    for (i in seq) {
+        if (hash[seq[i]]) {
+            if (hash[seq[i]][fragmentData[i]]) hash[seq[i]][fragmentData[i]]++; 
+            else hash[seq[i]][fragmentData[i]] = 1;
+        } else {
+            hash[seq[i]] = {};
+            hash[seq[i]][fragmentData[i]] = 1;
+        }
+    }
+          
+    for (i in hash) {  
+        char = '';
+        most = 0;
+        for (j in hash[i]) {
+            if (hash[i][j] > most) {
+                most = hash[i][j];
+                char = j;
             }
         }
-        
-        if (count <= n / 2) return ''; 
-        message[i] = current;
+        if (most <= n / 2) return '';
+        message += char;
     }
     
-    message = message.filter(n => n);
-    if (message.length == 1 && message[0] == '#') return '#';
-    if (message[0] == '#' || message[0] == '+') return '';
-    
-    return message.join('');
+    return (message[message.length-1] == '#' && message.length == mLength) ? message : '';  
 }
-
-
-
-
-
-/* OLDER VERSION */
-
-// function packetDescrambler(seq, fragmentData, n) {
-//     let goal, current, count;
-//     let unique = [...new Set(seq)].sort((a, b) => a - b);
-//     if (unique[unique.length - 1] - unique[0] + 1 != unique.length) return '';
-    
-//     let message = new Array(unique.length);
-
-//     let hash = {};
-//     for (let i = 0; i < seq.length; i++) {
-//         if (hash[seq[i]]) hash[seq[i]].push(fragmentData[i]);
-//         else hash[seq[i]] = [fragmentData[i]];
-//     }
-        
-//     for (i in hash) {
-//         goal = Math.ceil(hash[i].length / 2);        
-//         current = '';
-//         count = 0;
-        
-//         for (let j = 0; j < hash[i].length; j++) {
-//             if (j != 0) {
-//                 if (hash[i][j] == current) {
-//                     count++;
-//                     if (count == goal) break;
-//                 } else current = hash[i][j]; 
-//             } else {
-//                 current = hash[i][j];
-//                 count++;
-//             }
-//         }
-        
-//         if (count <= n / 2) return ''; 
-//         message[i] = current;
-//     }
-    
-//     if (message.length == 1 && message[0] == '#') return '#';
-//     if (message[0] == '#' || message[0] == '+') return '';
-    
-//     return message.join('');
-// }
