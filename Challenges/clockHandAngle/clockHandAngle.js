c98d1847ea9ae8eb43e438501c8da751a2390bf8
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-11-12 17:02:15 -0800

    updated clockHandAngle.js
    now passes all tests

--- Challenges/clockHandAngle/clockHandAngle.js
+++ Challenges/clockHandAngle/clockHandAngle.js
@@ -1,17 +1,15 @@
 function clockHandAngle(time) {
     time = time.split(':');
-    console.log(time);
     
     let seconds = 0;
-    seconds += (parseInt(time[0]) * 3600);
-    seconds += (parseInt(time[1]) * 60);
-    seconds += (parseInt(time[2]));
-    const hourhand = (seconds * (1/120)) % 360;
-    console.log(hourhand);
+    seconds += parseInt(time[0]) * 3600;
+    seconds += parseInt(time[1]) * 60;
+    seconds += parseInt(time[2]);
     
-    const minutehand = (parseInt(time[1]) + (parseInt(time[2]) / 60)) * 6;
-    console.log(minutehand);
+    const hour = (seconds * (1/120)) % 360;    
+    const minute = (parseInt(time[1]) + parseInt(time[2]) / 60) * 6;    
+    const difference = Math.abs(minute - hour);
     
-    return Math.abs(hourhand - minutehand);
-    
+    if (difference > 180) return 360 - difference;
+    else return difference;  
 }
