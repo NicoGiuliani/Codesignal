745fa60d7bd230bbc56b08196331e8a6e9f5ba8f
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2018-12-23 22:57:06 -0800

    Update burningCandles.js

--- Challenges/burningCandles/burningCandles.js
+++ Challenges/burningCandles/burningCandles.js
@@ -19,3 +19,22 @@ function burningCandles(candleSizes, n) {
     
     return total;
 }
+
+// least characters
+burningCandles = (c, n) => {
+    t = w = 0
+    c.sort((a, b) => b - a)
+    for (i = 0; c[i] > 0; i++) {        
+        w += c[i]
+        t += c[i]
+        c[i] = 0
+        
+        if ((i + 1) % n == 0) {
+            c.push(Math.floor(w / (n + 1)))
+            c.sort((a, b) => b - a)
+            i = -1
+            w = 0
+        }  
+    }
+    return t
+}
