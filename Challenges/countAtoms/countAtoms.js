7a1843dbebb2e4edc4d489d10814abeaf12692b6
NicoGiuliani <NicoGiuliani@users.noreply.github.com>
2019-01-19 19:58:42 -0800

    Update countAtoms.js

--- Challenges/countAtoms/countAtoms.js
+++ Challenges/countAtoms/countAtoms.js
@@ -1,58 +1,50 @@
 function countAtoms(molecule) {
+
     let molecules = [];
     let temp = molecule[0];
-
     for (let i = 1; i < molecule.length; i++) {
-        if (molecule[i].toUpperCase() === molecule[i]) {
+        if (molecule[i] == molecule[i].toUpperCase()) {
             molecules.push(temp);
             temp = molecule[i];
         } else temp += molecule[i];
-        // console.log(temp);
     }
     molecules.push(temp);
+    console.log(molecules);
+    
+    let multipliers = [1];
+    let lastMult = 1;
     molecules = molecules.reverse();
     console.log(molecules);
     
-    let multiplier = 1;
     for (let i = 0; i < molecules.length; i++) {
-        // if it's a number
         if (!isNaN(molecules[i])) {
-            multiplier = molecules[i]; 
+            lastMult *= molecules[i];
+            multipliers.push(lastMult);
             molecules[i] = '';
-        } else if (molecules[i] == ')') {
-            molecules[i] = '';
-            console.log("inside parenthesis");
-            let secondMult = -1;
+        } else if (molecules[i] == ')' || molecules[i] == ']') {
             for (let j = i + 1; true; j++) {
-                if (molecules[j] === '(') {
+                if (molecules[j] == '(' || molecules[j] == '[') {
+                    multipliers.pop();
+                    lastMult = multipliers[multipliers.length - 1];
                     i = j;
-                    molecules[i] = '';
                     break;
-                } else if (!isNaN(molecules[j])) {
-                    secondMult = multiplier * molecules[j];
-                    molecules[j] = '';
-                    // console.log(multiplier);
-                    continue;
-                }
-                console.log(molecules[j]);
-                if (secondMult > 0) {
-                    molecules[j] += secondMult;
-                    secondMult = -1;
+                } else if (!isNaN(molecules[j]) {
+                    lastMult *= molecules[i];
+                    multipliers.push(lastMult);
+                    molecules[i] = '';               
                 } else {
-                    molecules[j] += multiplier;    
+                    console.log("lastMult", lastMult);
+                    molecules[j] += lastMult;
+                    console.log(molecules[j]);
                 }
-                
             }
-            console.log("outside parenthesis");
-            multiplier = 1;
-            
+            console.log(molecules);
         } else {
-            console.log(multiplier);
-            molecules[i] += multiplier;
+            molecules[i] += lastMult;
         }
         
+        
     }
-    
     console.log(molecules);
         
 }
